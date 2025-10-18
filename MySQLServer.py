ae761097fed17f5171508dbfe6c07a8853d29e42
Elijah <125945721+Elijah-05@users.noreply.github.com>
2025-10-18 12:40:03 +0000

    mysql server file for creating database from python script

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,33 @@
 import mysql.connector
+from mysql.connector import Error
 
-mydb = mysql.connector.connect(
-    host="root",
-    
-)
+mydb = None
+mycursor = None
+try:
+    # Database connection details
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="sample",
+        database="alx_book_store"
+    )
+
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+
+        # Create a Database (if it doesn't exist)
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+        print("Database 'alx_book_store' created successfully!")
+        mydb.commit()
+
+except mysql.connector.Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+
+finally:
+    # Close connections
+    if mycursor is not None:
+        mycursor.close()
+    if mydb is not None:
+        mydb.close()
+
